58e34ba6135ccfecb605aefcb6bde8fc5d15794c
AS2502 <akash.sharma@excelra.com>
2022-04-03 16:42:45 +0530

    Add dropdown button in card

--- mock.js
+++ mock.js
@@ -10,6 +10,26 @@ function mycheckbox() {
 }
 
 
+
+function myFunction() {
+  document.getElementById("myDropdown").classList.toggle("show");
+}
+
+// Close the dropdown if the user clicks outside of it
+window.onclick = function(event) {
+  if (!event.target.matches('.dropbtn')) {
+    var dropdowns = document.getElementsByClassName("dropdown-content");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+      }
+    }
+  }
+}
+
+
 var xValu = [14,16,18,20,22,24,26,28,30,2,4,6];
 
 new Chart("multi_line", {
